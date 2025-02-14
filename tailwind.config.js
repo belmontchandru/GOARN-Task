67f463e78735e44b50eebb933e2d9e86b3b2e4d7
Belmont Chandru <belmont.chandru@gmail.com>
2025-02-14 09:27:05 +0530

    Add About Us and Update Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,19 @@ module.exports = {
       fontFamily: {
         roboto: ["Roboto", "sans-serif"],
       },
-
+      colors: {
+        'primaryblue':'#293264',
+        'secondaryblue':'#1e2653',
+        'lightblue':'#a6b5cd',
+        'paleblue':'#8ba0bb',
+        'tertiaryblue':'#d3dae6',
+        'primarygray':'#c1cade',
+        'secondarygray':'#f5f7fa',
+        'tertiarygray':'#e7f0fb',
+        'orange': '#ff8c00',
+        'white':'#ffffff',
+      },
     },
-    
   },
   plugins: [],
 }
